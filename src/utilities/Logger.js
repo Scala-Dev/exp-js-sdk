3876ac1f3ca097fabdc3534c1427d147d080d51a
James Dalessio <jrd261@gmail.com>
2015-07-13 16:00:43 -0400

    better node logging EXP-309

--- src/utilities/Logger.js
+++ src/utilities/Logger.js
@@ -64,13 +64,18 @@
 
 const Stream = function(logger, level, type) {
   this.write = function (message, payload) {
-    console[type]('[' + logger.name + ']:' + message, payload);
+    if (!console[type]) type = 'log';
+    if (payload) {
+      console[type]('[' + logger.name + ']:' + message, payload);
+    } else {
+      console[type]('[' + logger.name + ']:' + message);
+    }
   };
 };
 
 const Logger = function(options) {
   this.name = options.name || 'Default';
-  this.trace = new Stream(this, 0, 'trace').write;
+  this.trace = new Stream(this, 0, 'debug').write;
   this.debug = new Stream(this, 20, 'debug').write;
   this.info = new Stream(this, 40, 'info').write;
   this.warn = new Stream(this, 60, 'warn').write;
