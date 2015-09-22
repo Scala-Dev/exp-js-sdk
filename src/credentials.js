fc7f58ca227825b8ee576d225c10a2e2fda0c8ba
James Dalessio <jrd261@gmail.com>
2015-09-22 15:09:30 -0400

    Fix bug where token is always empty.

--- src/credentials.js
+++ src/credentials.js
@@ -98,8 +98,8 @@ module.exports.generateToken = () => {
         throw new Error('No credentials available.');
       }
       var header = JSON.stringify({ alg: 'HS256', 'typ': 'JWT' });
-      var body = JSON.stringify({ uuid: uuid });
-      var hmac = crypto.createHmac('sha256', secret);
+      var body = JSON.stringify({ uuid: locals.uuid });
+      var hmac = crypto.createHmac('sha256', locals.secret);
       var message = base64url.encode(header) + '.' + base64url.encode(body);
       return  message + '.' + base64url.encode(hmac.update(message).digest());
     });
