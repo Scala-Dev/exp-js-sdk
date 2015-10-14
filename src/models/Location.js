28bc34b4104459fc300f383f1e36383e226a4ee9
James Dalessio <jrd261@gmail.com>
2015-10-14 12:58:21 -0400

    Remove device/location zone methods.

--- src/models/Location.js
+++ src/models/Location.js
@@ -2,14 +2,8 @@
 
 module.exports = function (context) {
 
-  const api = require('../api');
-
   this.uuid = context.location.uuid;
   this.document = context.location;
-
-  this.getZones = () => {
-    return api.getZones({ locationUuid: context.location.uuid });
-  };
 
 };
 
