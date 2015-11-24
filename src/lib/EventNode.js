369346dd87968c8d231b534293f4469e66326f40
James Dalessio <jrd261@gmail.com>
2015-11-24 14:27:15 -0500

    Strap cancel method to listener.

--- src/lib/EventNode.js
+++ src/lib/EventNode.js
@@ -7,13 +7,20 @@ function EventNode () {
   this.on = (name, callback) => {
     if (!self.map[name]) self.map[name] = [];
     self.map[name].push(callback);
-    return () => self.map[name].splice(self.map[name].indexOf(callback), 1);
+    const cancel = () => { self.map[name].splice(self.map[name].indexOf(callback), 1); };
+    const cancel2 = () => { self.map[name].splice(self.map[name].indexOf(callback), 1); };
+    cancel.cancel = cancel2;
+    return cancel;
   };
 
   this.trigger = (name, payload) => {
     var promises = [];
     (self.map[name] || []).forEach(callback => promises.push(callback(payload)));
-    return Promise.all(promises);  
+    return Promise.all(promises);
+  };
+
+  this.clear = () => {
+    this.map = {};
   };
 
 }
