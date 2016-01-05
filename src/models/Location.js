2442a9bedfdfb679f06435d60dc938eeac426cda
Adam Galloway <adam.galloway@scala.com>
2016-01-05 12:49:05 -0500

    EXP-1231 added helper method to build url for location layout image

--- src/models/Location.js
+++ src/models/Location.js
@@ -2,8 +2,14 @@
 
 module.exports = function (context) {
 
+  const config = require('../config');
+
   this.uuid = context.location.uuid;
   this.document = context.location;
+
+  this.getLayoutUrl = () => {
+    return config.host + '/api/locations/' + this.uuid + '/layout';
+  };
 
 };
 
