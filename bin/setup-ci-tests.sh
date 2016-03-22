0e6907e44ad1c9dafeab78ec32564212f22267a5
James Dalessio <jrd261@gmail.com>
2016-03-22 13:34:04 -0400

    Fix sleep commands.

--- bin/setup-ci-tests.sh
+++ bin/setup-ci-tests.sh
@@ -8,14 +8,12 @@ git checkout develop
 npm install
 export NODE_ENV=test
 npm start&
-sleep(5)
+sleep 10
 cd ..
 git clone git@github.com:scalainc/exp-network.git
 cd exp-network
 npm install
 npm start&
-sleep(5)
+sleep 10
 cd ../exp-js-sdk
 npm run build
-
-
