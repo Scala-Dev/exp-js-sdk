e958f8d5b4a5c0a1a2486a6812e2a88b5873ff98
James Dalessio <jrd261@gmail.com>
2016-03-22 12:19:48 -0400

    Fix github archive command.

--- bin/setup-ci-tests.sh
+++ bin/setup-ci-tests.sh
@@ -4,7 +4,7 @@ set -e
 cd ..
 mkdir exp-api
 cd exp-api
-git archive --format=tar --remote=git@github.com:/scalainc/exp-api.git develop | tar xf -
+git archive --format=tar --remote=git@github.com:scalainc/exp-api.git develop | tar xf -
 npm install
 export NODE_ENV=test
 npm start&
@@ -12,7 +12,7 @@ sleep(5)
 cd ..
 mkdir exp-network
 cd exp-network
-git archive --format=tar --remote=git@github.com:/scalainc/exp-network.git develop | tar xf -
+git archive --format=tar --remote=git@github.com:scalainc/exp-network.git develop | tar xf -
 npm install
 npm start&
 sleep(5)
