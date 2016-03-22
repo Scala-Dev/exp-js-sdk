be37bbfea948fe41bc17d29e4d2ce7d6a67d7ffc
James Dalessio <jrd261@gmail.com>
2016-03-22 15:08:18 -0400

    Make sure we are using develop.

--- bin/setup-ci-tests.sh
+++ bin/setup-ci-tests.sh
@@ -5,12 +5,15 @@ cd ..
 git clone git@github.com:scalainc/exp-api.git
 cd exp-api
 git checkout develop
+git pull origin develop
 npm install
 NODE_ENV=test npm start&
 sleep 10
 cd ..
 git clone git@github.com:scalainc/exp-network.git
 cd exp-network
+git checkout develop
+git pull origin develop
 npm install
 npm start&
 sleep 10
