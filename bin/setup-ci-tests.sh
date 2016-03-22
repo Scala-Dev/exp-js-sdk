fa48e039cf43c583372023aa7df274b9fe62da1b
James Dalessio <jrd261@gmail.com>
2016-03-22 13:30:31 -0400

    Remove git paths. Trying with new key.

--- bin/setup-ci-tests.sh
+++ bin/setup-ci-tests.sh
@@ -2,7 +2,7 @@
 set -e
 
 cd ..
-git clone git@github.com:scalainc/exp-api.git exp-api
+git clone git@github.com:scalainc/exp-api.git
 cd exp-api
 git checkout develop
 npm install
@@ -10,11 +10,12 @@ export NODE_ENV=test
 npm start&
 sleep(5)
 cd ..
-git clone git@github.com:scalainc/exp-network.git exp-network
+git clone git@github.com:scalainc/exp-network.git
 cd exp-network
 npm install
 npm start&
 sleep(5)
 cd ../exp-js-sdk
+npm run build
 
 
